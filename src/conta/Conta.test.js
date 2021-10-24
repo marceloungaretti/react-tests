0cb9b3252b5f3015048c693f5bf98ddffb4227cd
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-24 17:41:18 -0300

    projeto finalizado

---
+++ src/conta/Conta.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Conta from "./Conta";
+import { render, screen, fireEvent } from '@testing-library/react'
+
+
+describe('Componente de Conta:', () => {
+    it('Exibir o saldo da conta com formatação monetária', () => {
+        render(<Conta saldo={1000}/>)
+        
+        const saldo = screen.getByTestId('saldo-conta');
+
+        expect(saldo.textContent).toBe('R$ 1000');
+    })
+
+    it('Chama a função de realizar transação quando o botão é clicado', () => {
+        const funcaoRealizarTransacao = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />)
+
+        fireEvent.click(screen.getByText('Realizar operação'));
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled();
+    });
+})
