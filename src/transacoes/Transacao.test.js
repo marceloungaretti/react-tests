73a633c321e01b0cdb7c6f434343161b251b086c
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-24 16:53:16 -0300

    terceiro capitulo concluido

---
+++ src/transacoes/Transacao.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Transacao from "./Transacao";
+
+describe('Componente de Transação do extrato:', () => {
+    it('O snapshot do componente deve permanecer sempre o mesmo',
+     () => {
+        const { container } = render(<Transacao 
+            data="08/09/2020"
+            tipo="saque"
+            valor="20"
+        />)
+
+         expect(container.firstChild).toMatchSnapshot();
+    })
+})
